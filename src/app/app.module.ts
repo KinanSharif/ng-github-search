48796dd2322c2ac36281664eb3ae86ef576d1d0d
kinan <kinansharif14@gmail.com>
2019-04-25 22:21:27 +0530

    user-listing component skeleton design done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { UserListingComponent } from './user-listing/user-listing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserListingComponent
   ],
   imports: [
     BrowserModule
