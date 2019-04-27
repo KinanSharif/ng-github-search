f93e811969c3f4a1fa69a69dbf2976d780b81ca2
kinan <kinansharif14@gmail.com>
2019-04-27 17:50:36 +0530

    minor styling fixes with color and margins

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,6 +12,7 @@ export class HeaderComponent implements OnInit {
   public searchUserName: string;
   public sortValue = 'nameAsc';
   disableIfNoResult = true;
+  public sortText = 'Sort By ';
 
 
   constructor(private userService: UserService) {
